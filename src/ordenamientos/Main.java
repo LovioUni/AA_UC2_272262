a9d37b01cf15f3c41aff10f3e8eacde5a9c10a7a
LovioUni <thelovio57@gmail.com>
2026-03-10 16:28:51 -0700

    se añadieron comentarios

--- src/ordenamientos/Main.java
+++ src/ordenamientos/Main.java
@@ -9,25 +9,28 @@ package ordenamientos;
  * @author Lenovo
  */
 public class Main {
-
+// Método para imprimir el arreglo
     public static void imprimirArreglo(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
+        for (int num : arr) {  // recorre todos los elementos
+            System.out.print(num + " ");  // Impresión
         }
-        System.out.println();
+        System.out.println();  // Salta linea
     }
 
     public static void main(String[] args) {
-
-        int[] miArreglo = {5, 4, 24, 3};
+        // Declaración e inicialización del arreglo
+        int[] miArreglo = {5, 4, 24, 3};  // Asignación
 
         System.out.println("Arreglo Original: ");
-        imprimirArreglo(miArreglo);
+        imprimirArreglo(miArreglo);  //Llamada al metodo
 
-        Ordenamientos.selectionSort(miArreglo);
-        // Ordenamientos.bubbleSort(miArreglo);
+        // Ordenamientos.selectionSort(miArreglo);
+        Ordenamientos.bubbleSort(miArreglo);   // llamada a método
 
         System.out.println("Arreglo Ordenado: ");
-        imprimirArreglo(miArreglo);
+        imprimirArreglo(miArreglo);   // llamada a método
     }
 }
+
+//Para cambiar de metodo de ordenamient solo hay que comentar y descomentar
+// las lineas 27  y 28
--- src/ordenamientos/Ordenamientos.java
+++ src/ordenamientos/Ordenamientos.java
@@ -9,39 +9,51 @@ package ordenamientos;
  * @author Lenovo
  */
 public class Ordenamientos {
-
+     // Metodo de ordenamiento Selection Sort
     public static void selectionSort(int[] arr) {
-        int n = arr.length;
+        int n = arr.length;  // tamaño del arreglo. 1 operación
 
-        for (int i = 0; i < n - 1; i++) {
-            int min = i;
+        // recorrer el arreglo
+        for (int i = 0; i < n - 1; i++) {  // 1+n+n
+            int min = i;   // se asume que el mínimo es la posición actual. 1
 
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
+            // buscar el elemento más pequeño en el resto del arreglo
+            for (int j = i + 1; j < n; j++) { // 1 + n + n
+                
+                if (arr[j] < arr[min]) {  // comparación. 1 
+                    min = j;    // actualizar posición del mínimo. 1
                 }
             }
 
-            int temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
+            // intercambio de elementos
+            int temp = arr[i]; // 1
+            arr[i] = arr[min]; // 1
+            arr[min] = temp; // 1
         }
     }
 
-
+    // Ordena el arreglo comparando pares de elementos adyacentes 
+    // e intercambiándolos si están en el orden incorrecto
     public static void bubbleSort(int[] arr) {
-        int n = arr.length;
+        int n = arr.length; // 1
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        // Este bucle externo controla el número de pasadas
+        for (int i = 0; i < n - 1; i++) {  // 1 + n + n
+            // Bucle interno, compara elementos adyacentes
+            for (int j = 0; j < n - i - 1; j++) { // 1 + n + n
 
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                // comparación entre elementos adyacentes
+                if (arr[j] > arr[j + 1]) {  // 1
+                    // intercambio de valores
+                    int temp = arr[j];      // 1
+                    arr[j] = arr[j + 1];    // 1
+                    arr[j + 1] = temp;      // 1
                 }
 
             }
         }
     }
 }
+
+// Ambos metodos de ordenamiento son O(n²) ya que se utilizan 2 ciclos for
+// esto hace que las operaciones crezca como n x n, osea n² cuando se aumenta el tamaño del arreglo.
